a5ff390f80bbd7dd779a219096b8500b90380a7d
pabloturati <pablo.turati@gmail.com>
2018-09-11 20:57:03 -0500

    cleaned

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,9 @@
 import React, { Component } from "react";
 
-/*
-  Note : Eacn <Project/> component will need to receive props from the FilterProjects
-         component to write the code to render:
-
-    + the project name inside the <span> ... </span>
-    + the appropriate className for a team project or solo project
-*/
-
 class Project extends Component {
   render() {
     const { projectName, solo } = this.props.project;
     const soloStr = solo ? "project--solo" : "project--team";
-
-    //const theProjectName = 'THE PROJECT NAME' // pass in value as props
-    // const soloProjectClassName = 'project--solo'
-    // const teamProjectClassName = 'project--team'
 
     return (
       <div className={`project ${soloStr}`}>
